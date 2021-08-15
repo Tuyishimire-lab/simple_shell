7a7c0b2d9ba661c3fd2a21e16f89f24d98886de0
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-08-15 07:01:14 -0400

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,8 +1,87 @@
 #include "shell.h"
-#include "ctrl_D.c"
-#include "cntrl_c.c"
-#include "builtin_hdl.c"
-#include "space_ignr.c"
+
+/**
+ * ctrl_c - ignore Ctrl-C input and prints prompt again
+ * @n: takes in int from signal
+ */
+void ctrl_c(int n)
+{
+        (void)n;
+        write(STDOUT_FILENO, "\n$ ", 3);
+}
+
+/**
+ * built_in - handles builtins (exit, env, cd)
+ * @token: user's typed command
+ * @env: environmental variable
+ * @num: take in nth user command typed to write error message
+ * @command: bring in command to free
+ * Return: 1 if acted on builtin, 0 if not
+ */
+int built_in(char **token, list_t *env, int num, char **command)
+{
+        int i = 0;
+
+        /* if user types "exit", free cmd tokens, and exit */
+        if (_strcmp(token[0], "exit") == 0)
+        {
+                i = __exit(token, env, num, command);
+        }
+        /* if user types "env", print, free cmd tokens, and reprompt */
+        else if (_strcmp(token[0], "env") == 0)
+        {
+                _env(token, env);
+                i = 1;
+        }
+        /* if user types "cd" , it will change directory */
+        else if (_strcmp(token[0], "cd") == 0)
+        {
+                i = _cd(token, env, num);
+        }
+        /* if user types "setenv", create or modify linked list node */
+        else if (_strcmp(token[0], "setenv") == 0)
+        {
+                _setenv(&env, token);
+                i = 1;
+        }
+        /* if user types "setenv", remove linked list node */
+        else if (_strcmp(token[0], "unsetenv") == 0)
+        {
+                _unsetenv(&env, token);
+                i = 1;
+        }
+        return (i);
+}
+
+/**
+ * ignore_space - return string without spaces in front
+ * @str: string
+ * Return: new string
+ */
+char *ignore_space(char *str)
+{
+        while (*str == ' ')
+                str++;
+        return (str);
+}
+
+/**
+ * ctrl_D - exits program if Ctrl-D was pressed
+ * @i: characters read via get_line
+ * @command: user's typed in command
+ * @env: environmental variable linked list
+ */
+void ctrl_D(int i, char *command, list_t *env)
+{
+        if (i == 0) /* handles Ctrl+D */
+        {
+                free(command); /* exit with newline if in shell */
+                free_linked_list(env);
+                if (isatty(STDIN_FILENO))/* ctrl+d prints newline */
+                        write(STDOUT_FILENO, "\n", 1);
+                exit(0);
+        }
+}
 
 /**
  * prompt - repeatedly prompts user and executes user's cmds if applicable
