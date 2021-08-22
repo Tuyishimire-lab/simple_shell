07d8fdac7665065c1a75558f9072814d75300bf5
amikable0179 <amarachukwu.ogbu1993@gmail.com>
2021-08-22 16:13:25 +0000

    white space tab issues

--- free_dptr.c
+++ free_dptr.c
@@ -6,13 +6,13 @@
  */
 void free_double_ptr(char **str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != NULL)
-        {
-                free(str[i]);
-                i++;
-        }
-        free(str);
+	while (str[i] != NULL)
+	{
+	free(str[i]);
+	i++;
+	}
+	free(str);
 }
 
