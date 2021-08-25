cdca0ac90e7d0b65d2cef8c1ce05d5f71d9619bb
Tuyishimire Ange Victorien <68068538+Tuyishimire-lab@users.noreply.github.com>
2021-08-25 12:00:09 +0200

    renaming and editing spacing

--- _execve.c
+++ _execve.c
@@ -28,38 +28,37 @@ int _execve(char **s, list_t *env, int num)
 	/* check if command is absolute path */
 	if (access(s[0], F_OK) == 0)
 	{
-	holder = s[0];
-	t = 1;
+		holder = s[0];
+		t = 1;
 	}
 	/* else flesh out full path */
 	else
-	holder = _which(s[0], env);
+		holder = _which(s[0], env);
 	/* if not an executable, free */
 	if (access(holder, X_OK) != 0)
 	{
-	not_found(s[0], num, env);
-	free_double_ptr(s);
-	return (127);
+		not_found(s[0], num, env);
+		free_double_ptr(s);
+		return (127);
 	}
 	else /* else fork and execute executable command */
 	{
-	pid = fork();
-	if (pid == 0) /* if child process, execute */
-	{
-	if (execve(holder, s, NULL) == -1)
-	{
-	not_found(s[0], num, env); /* special err msg */
-	c_exit(s, env);
-	}
-	}
-	else /* if parent, wait for child then free all */
-	{
-	wait(&status);
-	free_double_ptr(s);
-	if (t == 0)
-	free(holder);
-	}
+		pid = fork();
+		if (pid == 0) /* if child process, execute */
+		{
+			if (execve(holder, s, NULL) == -1)
+			{
+				not_found(s[0], num, env); /* special err msg */
+				c_exit(s, env);
+			}
+		}
+		else /* if parent, wait for child then free all */
+		{
+			wait(&status);
+			free_double_ptr(s);
+			if (t == 0)
+				free(holder);
+		}
 	}
 	return (0);
-	}
-
+}
